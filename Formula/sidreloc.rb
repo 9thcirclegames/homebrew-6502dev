f804ef9118ae9f8fd503d401bb2097ea6b6a1ed9
Gabriele Baldassarre <gabriele.baldassarre@mrm-mccann.com>
2020-04-14 21:47:21 +0200

    Changed SHA

--- Formula/sidreloc.rb
+++ Formula/sidreloc.rb
@@ -2,7 +2,7 @@ class Sidreloc < Formula
   desc "Reloacator for C64 SID files"
   homepage "https://www.linusakesson.net/software/sidreloc/index.php"
   url "https://github.com/9thcirclegames/sidreloc/archive/v1.0.tar.gz"
-  sha256 "90e35e295ceb69b18c7387433296a80b20b101e290aedfbd2902333506ae114c"
+  sha256 "896de78ee56de012e8fec8d15e7ead6a70d6ec3f69c611007dcd427af94c746c"
 
 def install
 
